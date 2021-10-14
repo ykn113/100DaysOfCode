c51c0fefc007c007e692ee615797ef58ad3cb5a8
ykn113 <yukinosnj13@gmail.com>
2021-10-14 15:21:53 +0900

    [Add] Day4

--- Treasure Map/main.py
+++ Treasure Map/main.py
@@ -8,7 +8,6 @@ position = input("Where do you want to put the treasure? ")
 column = int(position[0])
 row = int(position[1])
 
-selected_row = map[row-1]
-selected_row[column-1] = "X"
+map[row-1][column-1] = "X"
 
 print(f"{row1}\n{row2}\n{row3}")
