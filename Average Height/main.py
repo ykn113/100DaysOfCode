04c108df41875c090d06f9e47d64742cc61133d1
ykn113 <yukinosnj13@gmail.com>
2021-10-15 22:12:18 +0900

    [Add] Day5

---
+++ Average Height/main.py
@@ -0,0 +1,14 @@
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+    student_heights[n] = int(student_heights[n])
+
+total_height = 0
+for height in student_heights:
+    total_height += height
+
+number_of_student = 0
+for student in student_heights:
+    number_of_student += 1
+
+average_height = round(total_height / number_of_student)
+print(average_height)
