1e8e2d3fd7d24956993bd571de0cab4ac70e999c
ykn113 <yukinosnj13@gmail.com>
2021-10-19 15:39:04 +0900

    [Add] Day7 Step3

--- Hangman/main.py
+++ Hangman/main.py
@@ -1,22 +1,26 @@
 import random
-
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
 
 print(f'Pssst, the solution is {chosen_word}.')
 
 display = []
-word_length = len(chosen_word)
 for _ in range(word_length):
     display += "_"
 print(display)
+end_of_the_game = False
 
-guess = input("Guess a letter: ").lower()
+while not end_of_the_game:
+    guess = input("Guess a letter: ").lower()
 
-for position in range(word_length):
-    letter = chosen_word[position]
-    if letter == guess:
-        display[position] = letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+        print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        if letter == guess:
+            display[position] = letter
+    print(display)
 
-
-print(display)
+    if "_" not in display:
+        end_of_the_game = True
+        print("You win")
