8bd27eb887218a21d3709934452a1f4e18976a13
ykn113 <yukinosnj13@gmail.com>
2021-11-14 11:58:45 +0900

    [Update] Day10

--- Calculator/main.py
+++ Calculator/main.py
@@ -1,3 +1,4 @@
+from art import logo
 def add(n1, n2):
     return n1 + n2
 
@@ -11,28 +12,29 @@ def divide(n1, n2):
     return n1 / n2
 
 operations = {
-      "+": add,
-      "-": subtract,
-      "*": multiply,
-      "/": divide
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
 }
+def calculator():
+    print(logo)
+    num1 = float(input("What's the first number?: "))
+    for symbol in operations:
+        print(symbol)
+    should_continue = True
+    while should_continue:
 
-num1 = int(input("What's the first number?: "))
-for symbol in operations:
-    print(symbol)
+        operation_symbol = input("Pick an operation: ")
+        num2 = float(input("What's the next number?: "))
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
 
-operation_symbol = input("Pick an operation: ")
-num2 = int(input("What's the next number?: "))
-calculation_function = operations[operation_symbol]
-first_answer = calculation_function(num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
 
-print(f"{num1} {operation_symbol} {num2} = {first_answer}")
-
-operation_symbol = input("Pick an operation: ")
-num3 = int(input("What's the next number?: "))
-
-calculation_function = operations[operation_symbol]
-second_answer = calculation_function(calculation_function(num1, num2), num3)
-second_answer = calculation_function(first_answer, num3)
-
-print(f"{first_answer} {operation_symbol} {num3} = {second_answer}")
+        if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation .: ") == "y":
+            num1 = answer
+        else:
+            should_continue = False
+            calculator()
+calculator()
